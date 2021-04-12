e81d78b2299a51a92778f2ac6ac8f72ccd596d75
emanuele <emattina97@gmail.com>
2021-04-12 15:27:12 +0200

    Carico prova di collegamento vue e api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
 Vue.config.devtools = true;
 
-var app   = new Vue ({
+var app = new Vue ({
     el: '#root',
     data: {
-        
+        inputSearch: 'avatar',
+        movies: [
+            {
+                "title": "Natale in India",
+                "original_title": "Natale in India",
+                "vote_average": 4.8,
+                "overview": "Il giudice Enrico Paci e l'ingegner Fabio De Tassis si conoscono alla nursery nel 1987 dove sembra evidente che, a causa di un black out, i rispettivi figli maschi sono andati scambiati.",
+            }
+        ]
+    },
+    mounted() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=4117a3cbfe3896f4856820d20acc2358&query='+ this.inputSearch +'&language=it')
+        .then((results) => {
+            this.movies = (results.data.movies);
+        });
     },
     methods: {
+        searchMovie() {
 
+        }
     }
 });

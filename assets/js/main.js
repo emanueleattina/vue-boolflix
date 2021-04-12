e46f7c98875c68665eba4a20683fbc865628fbf4
emanuele <emattina97@gmail.com>
2021-04-12 16:33:17 +0200

    Carico ricerca funzionante e bandiere

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,30 @@ var app = new Vue ({
     },
     methods: {
         searchMovie() {
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=4117a3cbfe3896f4856820d20acc2358&query='+ this.inputSearch)
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=4117a3cbfe3896f4856820d20acc2358&query='+ this.inputSearch +'&language=it-IT')
             .then((results) => {
                 this.movies = (results.data.results);
-        });
+            });
+        },
+        langFlag() {
+            for(i = 0; i < this.movies.length; i++) {
+                if(this.movies[i].original_language == "en") {
+                    this.movies[i].original_language = "🇬🇧";
+                }
+                if(this.movies[i].original_language == "it") {
+                    this.movies[i].original_language = "🇮🇹";
+                }
+                if(this.movies[i].original_language == "es") {
+                    this.movies[i].original_language = "🇪🇸";
+                }
+                if(this.movies[i].original_language == "fr") {
+                    this.movies[i].original_language = "🇫🇷";
+                }
+                if(this.movies[i].original_language == "de") {
+                    this.movies[i].original_language = "🇩🇪";
+                }
+
+            }
         }
     }
 });

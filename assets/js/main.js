c88735900c4f8c169b4979022b073c824c3ce364
emanuele <emattina97@gmail.com>
2021-04-12 16:53:28 +0200

    Carico ricerca serie tv e film divisi

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,26 @@ var app = new Vue ({
     el: '#root',
     data: {
         inputSearch: '',
-        movies: []
+        movies: [],
+        series: [],
     },
     methods: {
+        searchContent () {
+            this.searchMovie();
+            this.searchSerie();
+        },
         searchMovie() {
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=4117a3cbfe3896f4856820d20acc2358&query='+ this.inputSearch +'&language=it-IT')
             .then((results) => {
                 this.movies = (results.data.results);
+                this.langFlag();
+            });
+        },
+        searchSerie() {
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=4117a3cbfe3896f4856820d20acc2358&language=it_IT&query='+ this.inputSearch)
+            .then((results) => {
+                this.series = (results.data.results);
+                this.langFlag();
             });
         },
         langFlag() {
@@ -29,6 +42,24 @@ var app = new Vue ({
                 }
                 if(this.movies[i].original_language == "de") {
                     this.movies[i].original_language = "🇩🇪";
+                }
+            }
+
+            for(i = 0; i < this.series.length; i++) {
+                if(this.series[i].original_language == "en") {
+                    this.series[i].original_language = "🇬🇧";
+                }
+                if(this.series[i].original_language == "it") {
+                    this.series[i].original_language = "🇮🇹";
+                }
+                if(this.series[i].original_language == "es") {
+                    this.series[i].original_language = "🇪🇸";
+                }
+                if(this.series[i].original_language == "fr") {
+                    this.series[i].original_language = "🇫🇷";
+                }
+                if(this.series[i].original_language == "de") {
+                    this.series[i].original_language = "🇩🇪";
                 }
 
             }
